91293c057c89f35e5260515140e4e0bd0bffe8c4
Alan <ddouble.cn@gmail.com>
2023-04-05 10:39:45 +0200

    Initial Commit

---
+++ ZTranslator/ZTranslatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZTranslatorApp.swift
+//  ZTranslator
+//
+//  Created by Alan Zhou on 5/4/23.
+//
+
+import SwiftUI
+
+@main
+struct ZTranslatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
