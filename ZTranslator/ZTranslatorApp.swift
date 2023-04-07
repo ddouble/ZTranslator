53922175cdb47100da1a5863ee640afdbc37b2e8
Alan <ddouble.cn@gmail.com>
2023-04-07 15:15:42 +0200

    Translate selected text by openai api

--- ZTranslator/ZTranslatorApp.swift
+++ ZTranslator/ZTranslatorApp.swift
@@ -11,6 +11,7 @@
 import SwiftUI
 import Cocoa
 import MASShortcut
+import Foundation
 
 /**
  Get selected text from any running App
@@ -74,6 +75,58 @@ func sendTextToClipboard() {
     NSPasteboard.general.setString("Text to copy", forType: .string)
 }
 
+/**
+ Get tranlsation result from chatgpt
+ - Parameters:
+   - messages:
+   - completion:
+ */
+func getOpenAIResponse(messages: [[String: Any]], completion: @escaping (String?, Error?) -> Void) {
+    let apiKey = "YOUR-OPENAI-API-KEY"
+    let urlString = "https://api.openai.com/v1/chat/completions"
+    let url = URL(string: urlString)!
+    var request = URLRequest(url: url)
+    request.httpMethod = "POST"
+    request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+    request.addValue("Bearer \(apiKey)", forHTTPHeaderField: "Authorization")
+
+    let parameters: [String: Any] = [
+        "model": "gpt-3.5-turbo",
+        "messages": messages,
+        "temperature": 0.7,
+        "max_tokens": 1000
+    ]
+    request.httpBody = try! JSONSerialization.data(withJSONObject: parameters)
+
+    let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
+        if let error = error {
+            completion(nil, error)
+            return
+        }
+        guard let data = data else {
+            completion(nil, nil)
+            return
+        }
+        do {
+            let json = try JSONSerialization.jsonObject(with: data, options: []) as? [String: Any]
+            if let error = json?["error"] as? [String: Any] {
+                let message = error["message"] as? String ?? "Unknown error"
+                let type = error["type"] as? String ?? ""
+                let error = NSError(domain: "OpenAIError", code: 0, userInfo: [NSLocalizedDescriptionKey: "\(type): \(message)"])
+                completion(nil, error)
+            } else {
+                let choices = json?["choices"] as? [[String: Any]]
+                let message = choices?.first?["message"] as? [String: Any]
+                let text = message?["content"] as? String
+                completion(text, nil)
+            }
+        } catch {
+            completion(nil, error)
+        }
+    }
+    task.resume()
+}
+
 
 extension Notification.Name {
     static let selectedTextChanged = Notification.Name("SelectedTextChanged")
@@ -93,7 +146,25 @@ class ZTranslatorApp: App {
         let shortcut = MASShortcut(keyCode: kVK_ANSI_X, modifierFlags: [.control, .command])
         MASShortcutMonitor.shared().register(shortcut) {
             let text = getSelectedText()
-            NotificationCenter.default.post(name: .selectedTextChanged, object: text)
+            let messages: [[String: Any]] = [
+                ["role": "user", "content": "translate to japanese：" + (text ?? "")],
+//                ["role": "assistant", "content": "Hi there, how can I help you today?"],
+//                ["role": "user", "content": "I need help with a problem"],
+//                ["role": "assistant", "content": "Sure, what kind of problem are you experiencing?"],
+            ]
+            getOpenAIResponse(messages: messages) { (response, error) in
+                if let error = error {
+                    print("Error: \(error)")
+                    NotificationCenter.default.post(name: .selectedTextChanged, object: error)
+                } else if let response = response {
+                    print("Response: \(response)")
+                    NotificationCenter.default.post(name: .selectedTextChanged, object: response)
+                } else {
+                    print("No response")
+                    NotificationCenter.default.post(name: .selectedTextChanged, object: "No response")
+                }
+            }
+//            NotificationCenter.default.post(name: .selectedTextChanged, object: text)
         }
     }
 }
